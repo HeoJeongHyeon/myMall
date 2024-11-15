5b7f37fe8663c5f28e3ea1cef512f2b8bd2677f5
HeoJeongHyeon <jh7e0n20@gmail.com>
2024-11-15 18:07:21 +0900

    initial commit

---
+++ src/main/java/my/mydev/MyDevApplication.java
@@ -0,0 +1,13 @@
+package my.mydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/mydev/MyDevApplicationTests.java
@@ -0,0 +1,13 @@
+package my.mydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
